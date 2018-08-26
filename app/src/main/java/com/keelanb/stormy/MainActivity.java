39ef1f2dc92e53a2feda4b7fab14954b9d4604f0
Keelan Byrne <keelanb542@gmail.com>
2018-08-26 22:56:55 +0100

    Initial commit

---
+++ app/src/main/java/com/keelanb/stormy/MainActivity.java
@@ -0,0 +1,13 @@
+package com.keelanb.stormy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
