398321db41ad50610a1377a528ae9914a0b44809
Keelan Byrne <keelanb542@gmail.com>
2018-08-30 13:55:53 +0100

    Made test request to Dark Sky API the WRONG way

--- app/src/main/java/com/keelanb/stormy/MainActivity.java
+++ app/src/main/java/com/keelanb/stormy/MainActivity.java
@@ -1,13 +1,47 @@
 package com.keelanb.stormy;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String apiKey = "2a6473b98d11fc375c3068d0f691a876";
+
+        double latitude = 37.8267;
+        double longitude = -122.4233;
+
+        String forecastURL = "https://api.darksky.net/forecast/" + apiKey + "/" + latitude + "," + longitude;
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(forecastURL)
+                .build();
+
+        Call call = client.newCall(request);
+
+        try {
+            Response response = call.execute();
+            if (response.isSuccessful()) {
+                Log.v(TAG, response.body().string());
+            }
+        } catch (IOException e) {
+            Log.e(TAG, "IO exception caught: ", e);
+        }
     }
 }
