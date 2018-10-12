456bc983577bf420ef7bfa0813c2594c7ef84c17
Keelan Byrne <keelanb542@gmail.com>
2018-10-12 19:24:10 +0100

    update list item layout

--- app/src/main/java/com/keelanb/stormy/Weather/Forecast.java
+++ app/src/main/java/com/keelanb/stormy/Weather/Forecast.java
@@ -1,5 +1,7 @@
 package com.keelanb.stormy.Weather;
 
+import com.keelanb.stormy.R;
+
 public class Forecast {
     private Current current;
     private Hour[] hourlyForecast;
@@ -18,5 +20,38 @@ public class Forecast {
 
     public void setHourlyForecast(Hour[] hourlyForecast) {
         this.hourlyForecast = hourlyForecast;
+    }
+
+    public static int getIconId(String iconString) {
+        int iconId = R.drawable.clear_day;
+
+        switch (iconString) {
+            case "rain":
+                iconId = R.drawable.rain;
+                break;
+            case "snow":
+                iconId = R.drawable.snow;
+                break;
+            case "sleet":
+                iconId = R.drawable.sleet;
+                break;
+            case "wind":
+                iconId = R.drawable.wind;
+                break;
+            case "fog":
+                iconId = R.drawable.fog;
+                break;
+            case "cloudy":
+                iconId = R.drawable.cloudy;
+                break;
+            case "partly-cloudy-day":
+                iconId = R.drawable.partly_cloudy;
+                break;
+            case "partly-cloudy-night":
+                iconId = R.drawable.cloudy_night;
+                break;
+        }
+
+        return iconId;
     }
 }
